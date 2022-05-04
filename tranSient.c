d9f36af26db81d783c806a5a204a9a17cfe2a85d
aleliar <76943818+aleliar@users.noreply.github.com>
2022-05-04 10:24:58 +0800

    Add files via upload

---
+++ tranSient.c
@@ -0,0 +1,49 @@
+/***********************************************************************
+    UDF for transition state simulation inlet profile boundary condition and fuel mass inlet profile boundary condition.
+***************************************************************************/
+
+#include "udf.h"
+#include "unsteady.h"
+
+/* Mass flow inlet */
+DEFINE_PROFILE(transient_mdot, thread, position)
+{
+    real flow_time = CURRENT_TIME;
+    real m1 = 0.3336; real m2 = 0.5004;
+    real mdot;
+    face_t f;
+
+    begin_f_loop(f, thread)
+    {
+        F_PROFILE(f,thread,position) = (m1+(m2-m1)*flow_time/0.6)/12;
+    }
+    end_f_loop(f,thread)
+}
+
+/*Pressure outlet */
+DEFINE_PROFILE(transient_pressure, thread, position)
+{
+    real flow_time = CURRENT_TIME;
+    real p1 = 112940; real p2 = 121560;
+    real pout;
+    face_t f;
+
+    begin_f_loop(f, thread)
+    {
+        F_PROFILE(f,thread,position) = p1+(p2-p1)*flow_time/0.6;
+    }
+    end_f_loop(f,thread)
+}
+
+/* transient injection flow rate*/
+DEFINE_DPM_INJECTION_INIT(fuel,I)
+{
+    Particle *p;
+    real flow_time = RP_Get_Real("flow-time");
+    real fuel_m1=0.012667, fuel_m2=0.014057;
+
+    loop(p,I->p_init)
+    {
+        p->flow_rate =   (fuel_m1+(fuel_m2-fuel_m1)*flow_time/0.6)/12;
+    }
+}
